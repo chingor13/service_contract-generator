ad63d18d86d031baeff565e891078fd5a1ea7a5f
Jeff Ching <jching@avvo.com>
2014-09-03 13:33:00 -0700

    generator cli

--- lib/service_contract/generator/cli.rb
+++ lib/service_contract/generator/cli.rb
@@ -28,13 +28,45 @@ module ServiceContract
           `#{command}`
         end
 
+        # add service_contract to .gemspec
+        gemspec = "#{contract_name}/#{contract_name}.gemspec"
+        dependency_line = `grep -m 1 -n '.add_de' #{gemspec}`
+        matched = false
+        output = ""
+        File.open(gemspec, "r") do |file|
+          file.each_line do |line|
+            puts line
+            if !matched && match = line.match(/([^\.]+)\.add_de/)
+              puts "matched"
+              output += %{#{match[1]}.add_dependency "service_contract"\n}
+              matched = true
+            end
+            output += line
+          end
+        end
+        puts output
+        File.open(gemspec, "w") do |file|
+          file.write(output)
+        end
+        # if match = dependency_line.match(/(\d+):([^\.]+)\.add_de/)
+        #   line_number = match[1]
+        #   new_line = %{#{match[2]}.add_dependency "service_contract"}
+        #   gemspec_contents = ""
+
+        #   puts new_line
+        #   command = %{sed -i '#{line_number}i\\ #{new_line}' #{gemspec}}
+        #   puts command
+        #   # `#{command}`
+        # end
+
         # template files
-        path_to_root = "PATH/TO/ROOT"
+        path_to_root = (Array.new(2 + module_name.split("::").length) {".."}).join("/")
         service_name = ActiveSupport::Inflector.humanize(contract_name)
         b = binding()
         Dir.glob(File.expand_path("../../../../template/*.erb", __FILE__)).each do |template|
           erb = ERB.new(File.read(template))
           output_file = File.join(contract_name, 'lib', path, File.basename(template, ".erb"))
+          puts "writing template: #{output_file}"
           File.open(output_file, 'w') do |file|
             file.write(erb.result(b))
           end
--- service_contract-generator.gemspec
+++ service_contract-generator.gemspec
@@ -19,7 +19,8 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency "thor"
+  spec.add_dependency "activesupport"
   spec.add_dependency "bundler"
-  
+
   spec.add_development_dependency "rake", "~> 10.0"
 end
